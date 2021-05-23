451f6bb46edf48d186c7abf72b30bb4ea52498a8
Shaps Benkau <shapsuk@me.com>
2021-05-23 17:49:29 +0100

    Adds concept of a chart

--- Sources/Charts/ChartReader.swift
+++ Sources/Charts/ChartReader.swift
@@ -1,28 +1,39 @@
 import SwiftUI
 
-struct ChartProxy {
-    var size: CGSize
-}
+struct SingleValueChart<Data, Content: View>: View where Data: RandomAccessCollection, Data.Element: BinaryFloatingPoint {
 
-struct ChartReader<Content: View>: View {
+    let data: Data
+    let range: ClosedRange<Data.Element>
+    let content: (ChartProxy) -> Content
 
-    private let content: (ChartProxy) -> Content
-
-    init(@ViewBuilder _ content: @escaping (ChartProxy) -> Content) {
+    init(data: Data, in range: ClosedRange<Data.Element>? = nil, @ViewBuilder _ content: @escaping (ChartProxy) -> Content) {
+        let min = data.min() ?? 0
+        let max = data.max() ?? 0
+        self.data = data
+        self.range = range ?? min...max
         self.content = content
     }
 
     var body: some View {
         GeometryReader { geo in
-            content(ChartProxy(size: geo.size))
+            content(ChartProxy(size: geo.size, dataset: dataset(in: geo.frame(in: .local))))
         }
     }
+
+    private func dataset(in rect: CGRect) -> [Value] {
+        []
+    }
+
 }
 
-struct ChartReader_Previews: PreviewProvider {
-    static var previews: some View {
-        ChartReader { chart in
-            Text("\(chart.size.width)")
-        }
+extension SingleValueChart {
+    struct Value {
+        var unitPoint: UnitPoint
+        var rawValue: Data.Element
+    }
+
+    struct ChartProxy {
+        var size: CGSize
+        var dataset: [Value]
     }
 }
