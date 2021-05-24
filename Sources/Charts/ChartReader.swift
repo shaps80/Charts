c08604e61b37f158ec6f8b30dcff8fc9170b421a
Shaps Benkau <shapsuk@me.com>
2021-05-24 09:28:21 +0100

    Checkpoint

--- Sources/Charts/ChartReader.swift
+++ Sources/Charts/ChartReader.swift
@@ -1,39 +1,39 @@
 import SwiftUI
 
-struct SingleValueChart<Data, Content: View>: View where Data: RandomAccessCollection, Data.Element: BinaryFloatingPoint {
+struct Chart<Data, Content: View>: View where Data: RandomAccessCollection, Data.Element: Comparable {
 
-    let data: Data
-    let range: ClosedRange<Data.Element>
-    let content: (ChartProxy) -> Content
+    struct ChartProxy {
+        var size: CGSize
+        var values: Data.Element
+    }
+
+    let content: (Any) -> Content
+
+    init(data: Data, in range: ClosedRange<Data.Element>? = nil, @ViewBuilder _ content: @escaping (ChartProxy) -> Content) {
+        let min = data.min() ?? 0
+        let max = data.max() ?? 1
+        let values = data.map {
+            Value(rawValue: $0, range: <#T##ClosedRange<Comparable>#>)
+            Value(unitValue: CGFloat(($0 - min) / (max - min)), rawValue: $0)
+        }
+    }
+
+}
+
+extension Chart where Data.Element: BinaryFloatingPoint {
+
 
     init(data: Data, in range: ClosedRange<Data.Element>? = nil, @ViewBuilder _ content: @escaping (ChartProxy) -> Content) {
         let min = data.min() ?? 0
         let max = data.max() ?? 0
-        self.data = data
+        self.data = data.map(to: min...max)
         self.range = range ?? min...max
         self.content = content
     }
 
     var body: some View {
         GeometryReader { geo in
-            content(ChartProxy(size: geo.size, dataset: dataset(in: geo.frame(in: .local))))
+//            content(ChartProxy(size: geo.size, values: values(in: geo.frame(in: .local))))
         }
-    }
-
-    private func dataset(in rect: CGRect) -> [Value] {
-        []
-    }
-
-}
-
-extension SingleValueChart {
-    struct Value {
-        var unitPoint: UnitPoint
-        var rawValue: Data.Element
-    }
-
-    struct ChartProxy {
-        var size: CGSize
-        var dataset: [Value]
     }
 }
